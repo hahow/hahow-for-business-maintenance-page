dc030be5e7e9febeba09b505d329e826578cd403
Amo Wu <amo260@gmail.com>
2021-02-10 21:18:24 +0800

    style: npm run format

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,16 +8,16 @@ module.exports = {
   pathPrefix: "/hahow-for-business-maintenance-page",
   plugins: [
     {
-      resolve: 'gatsby-plugin-intercom-spa',
+      resolve: "gatsby-plugin-intercom-spa",
       options: {
-        app_id: 'cjhsnczh',
+        app_id: "cjhsnczh",
         include_in_development: true,
-        delay_timeout: 0
-      }
+        delay_timeout: 0,
+      },
     },
-    'gatsby-plugin-react-helmet'
+    "gatsby-plugin-react-helmet",
   ],
   siteMetadata: {
-    title: "Hahow for Business | 數位時代潛力人才的高效學習方案"
-  }
+    title: "Hahow for Business | 數位時代潛力人才的高效學習方案",
+  },
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,37 +8,43 @@ export default function Home() {
         <title>Hahow for Business | 數位時代潛力人才的高效學習方案</title>
         <link rel="icon" href="./favicon.ico?v=1.0" />
       </Helmet>
-      <div style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'column',
-        height: '100vh',
-      }}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+          height: "100vh",
+        }}
+      >
         <img
           src="./img-503.png"
           srcset="./img-503@2x.png 2x, ./img-503@3x.png 3x"
         />
-        <div style={{
-          fontSize: 30,
-          fontWeight: 600,
-          fontStretch: 'normal',
-          fontStyle: 'normal',
-          lineHeight: 1.27,
-          letterSpacing: 'normal',
-          color: 'rgba(0, 0, 0, 0.85)',
-        }}>
+        <div
+          style={{
+            fontSize: 30,
+            fontWeight: 600,
+            fontStretch: "normal",
+            fontStyle: "normal",
+            lineHeight: 1.27,
+            letterSpacing: "normal",
+            color: "rgba(0, 0, 0, 0.85)",
+          }}
+        >
           系統維護
         </div>
-        <div style={{
-          fontSize: 16,
-          fontWeight: 'normal',
-          fontStretch: 'normal',
-          fontStyle: 'normal',
-          lineHeight: 1.63,
-          letterSpacing: 'normal',
-          color: 'rgba(0, 0, 0, 0.65)',
-        }}>
+        <div
+          style={{
+            fontSize: 16,
+            fontWeight: "normal",
+            fontStretch: "normal",
+            fontStyle: "normal",
+            lineHeight: 1.63,
+            letterSpacing: "normal",
+            color: "rgba(0, 0, 0, 0.65)",
+          }}
+        >
           網站將於維修完畢後盡快上線，不便之處敬請包涵
         </div>
       </div>
